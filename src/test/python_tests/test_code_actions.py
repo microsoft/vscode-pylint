183834bfa328a903c0ef28c72fdb35230df86250
jjmacklin <jmacklin@seas.upenn.edu>
2022-09-20 14:19:48 -0400

    Remove imports from server.py and consolidate tests

--- src/test/python_tests/test_code_actions.py
+++ src/test/python_tests/test_code_actions.py
@@ -20,22 +20,9 @@ LINTER = utils.get_server_info_defaults()["name"]
     ("code", "contents", "command"),
     [
         (
-            "C0301:line-too-long",
+            "C0301:line-too-long", "C0305:trailing-newlines",
             # # pylint: disable=line-too-long
             "FRUIT = ['apricot', 'blackcurrant', 'cantaloupe', 'dragon fruit', 'elderberry', 'fig', 'grapefruit']",
-            {
-                "title": f"{LINTER}: Run document formatting",
-                "command": "editor.action.formatDocument",
-                "arguments": None,
-            },
-        )
-    ],
-)
-@pytest.mark.parametrize(
-    ("code", "contents", "command"),
-    [
-        (
-            "C0305:trailing-newlines",
             # # pylint: disable=trailing-newlines
             "VEGGIE = ['carrot\n', 'radish\n', 'cucumber\n', 'potato\n', '\n']",
             {

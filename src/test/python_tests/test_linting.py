0307aa21fbded67a3f4dd4387350c39394292786
Karthik Nadig <kanadig@microsoft.com>
2023-10-12 11:36:52 -0700

    Add stdlib filtering test

--- src/test/python_tests/test_linting.py
+++ src/test/python_tests/test_linting.py
@@ -4,6 +4,7 @@
 Test for linting over LSP.
 """
 
+import pathlib
 import os
 import sys
 from threading import Event
@@ -476,3 +477,99 @@ def test_severity_setting(lint_code):
     }
 
     assert_that(actual, is_(expected))
+
+
+@pytest.mark.parametrize("value", [True, False], ids=["filtering-on", "filtering-off"])
+def test_stdlib_filtering(value: bool):
+    """Test to ensure linting on file open."""
+    contents = TEST_FILE_PATH.read_text(encoding="utf-8")
+
+    actual = []
+    with session.LspSession() as ls_session:
+        init_options = defaults.VSCODE_DEFAULT_INITIALIZE["initializationOptions"]
+        init_options["settings"][0]["stdlibFiltering"] = value
+        ls_session.initialize(defaults.VSCODE_DEFAULT_INITIALIZE)
+
+        # trick pylint into thinking the file is stdlib file
+        FAKE_TEST_URI = utils.as_uri(
+            str(pathlib.Path(pytest.__file__).parent / "sample.py")
+        )
+
+        done = Event()
+
+        def _handler(params):
+            nonlocal actual
+            actual = params
+            done.set()
+
+        ls_session.set_notification_callback(session.PUBLISH_DIAGNOSTICS, _handler)
+
+        ls_session.notify_did_open(
+            {
+                "textDocument": {
+                    "uri": FAKE_TEST_URI,
+                    "languageId": "python",
+                    "version": 1,
+                    "text": contents,
+                }
+            }
+        )
+
+        # wait for some time to receive all notifications
+        done.wait(TIMEOUT)
+
+    expected = {
+        "uri": FAKE_TEST_URI,
+        "diagnostics": [
+            {
+                "range": {
+                    "start": {"line": 0, "character": 0},
+                    "end": {"line": 0, "character": 0},
+                },
+                "message": "Missing module docstring",
+                "severity": 3,
+                "code": "C0114:missing-module-docstring",
+                "codeDescription": {
+                    "href": f"{DOCUMENTATION_HOME}/convention/missing-module-docstring.html"
+                },
+                "source": LINTER["name"],
+            },
+            {
+                "range": {
+                    "start": {"line": 2, "character": 6},
+                    "end": {
+                        "line": 2,
+                        "character": 7 if sys.version_info >= (3, 8) else 6,
+                    },
+                },
+                "message": "Undefined variable 'x'",
+                "severity": 1,
+                "code": "E0602:undefined-variable",
+                "codeDescription": {
+                    "href": f"{DOCUMENTATION_HOME}/error/undefined-variable.html"
+                },
+                "source": LINTER["name"],
+            },
+            {
+                "range": {
+                    "start": {"line": 0, "character": 0},
+                    "end": {
+                        "line": 0,
+                        "character": 10 if sys.version_info >= (3, 8) else 0,
+                    },
+                },
+                "message": "Unused import sys",
+                "severity": 2,
+                "code": "W0611:unused-import",
+                "codeDescription": {
+                    "href": f"{DOCUMENTATION_HOME}/warning/unused-import.html"
+                },
+                "source": LINTER["name"],
+            },
+        ],
+    }
+
+    if value:
+        assert_that(actual, is_({"uri": FAKE_TEST_URI, "diagnostics": []}))
+    else:
+        assert_that(actual, is_(expected))
